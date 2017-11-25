050442a94b5fb6aa19573c60e883afe13b9c250c
Drew <dwarwick96@gmail.com>
2017-11-24 19:42:01 -0500

    Fixed setup file

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
-from setuptools import setup
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname), 'rt').read()
 
 setup(
-	name = "pyroblox",
-	version = "0.1",
-	description = "",
+	name = "pyRoblox",
+	version = "0.0.1",
+	description = "Python library for interfacing with Roblox",
+	long_description = read("README.md"),
 	url = "https://github.com/EchoReaper/pyroblox",
 	author = "Drew Warwick",
 	author_email = "dwarwick96@gmail.com",
 	license = "Apache 2.0",
-	packages=["pyroblox"],
+	packages = find_packages(exclude=["tests"]),
 	install_requires = [
-		"requests","bs4","python-dateutil"
-	],
-	zip_safe=False
+		"requests", "bs4", "python-dateutil"
+	]
 )
